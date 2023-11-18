521b32ed8dd8d27bf6cce0c8c0e2983a361a5d61
DESKTOP-D04QR97\Jesus <jesusmgit97@gmailcom>
2023-11-18 12:02:09 +0100

    first commit

---
+++ src/main/java/com/Aqua404/AutoDBbackend/AutoDbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Aqua404.AutoDBbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoDbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Aqua404/AutoDBbackend/AutoDbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Aqua404.AutoDBbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoDbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
